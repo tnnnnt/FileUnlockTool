25ee7e12eb6debed0a10c930eab4fe86c7c5117e
tnnnnt <cyz2324141281@163.com>
2024-09-22 14:27:09 +0800

    测试密码时仅对压缩包中的某个文件测试，节省大量时间，效率upupup！

--- FileUnlockTool/main.cpp
+++ FileUnlockTool/main.cpp
@@ -11,32 +11,64 @@ static void print(const std::string& s) {
 static void print(const int& a) {
 	std::cout << a << std::endl;
 }
-static bool try_password(const std::string& sevenzip_path, const std::string& file, const std::string& password) {
-	std::string cmd = "\"" + sevenzip_path + " x -o\"" + file + "~\" -p\"" + password + "\" \"" + file + "\" > NUL 2>&1\"";
+static std::string get_one_file_from_zip(const std::string& sevenzip_path, const std::string& zip_file) {
+	const std::string command = "\"" + sevenzip_path + " l \"" + zip_file + "\"\"";
+	std::string output = "";
+	char buffer[128];
+	// 打开管道
+	FILE* pipe = _popen(command.c_str(), "r");
+	if (!pipe) {
+		std::cerr << "popen() failed!" << std::endl;
+		return "";
+	}
+	// 读取命令的输出到buffer
+	while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
+		output += buffer;
+	}
+	// 关闭管道
+	_pclose(pipe);
+	std::stringstream output_stream(output);
+	std::string line;
+	while (getline(output_stream, line)) {
+		if (line.length() > 53 && line.substr(20, 5) == "....A") {
+			return line.substr(53);
+		}
+	}
+	return "";
+}
+static bool try_password(const std::string& sevenzip_path, const std::string& file, const std::string& password, const std::string& one_file) {
+	std::string cmd = "\"" + sevenzip_path + " x -o\"" + file + "~\" -p\"" + password + "\" -y \"" + file + "\" \"" + one_file + "\" > NUL 2>&1\"";
 	print(cmd);
 	int exit_code = system(cmd.c_str());
 	if (!exit_code) {
 		print(file + " success!\npassword is \"" + password + "\"");
 		return true;
 	}
-	else if (std::filesystem::exists(file + "~")) {
-		std::filesystem::remove_all(file + "~");
-	}
 	return false;
 }
-static bool building_brute_password(const std::string& sevenzip_path, const std::string& chars, const std::string password, const size_t& len, const std::string& file) {
+static bool building_brute_password(const std::string& sevenzip_path, const std::string& chars, const std::string password, const size_t& len, const std::string& file, const std::string& one_file, std::string& final_password) {
 	if (password.size() == len) {
-		return try_password(sevenzip_path, file, password);
+		final_password = password;
+		return try_password(sevenzip_path, file, password, one_file);
 	}
 	for (const auto& c : chars) {
-		if (building_brute_password(sevenzip_path, chars, password + c, len, file)) {
+		if (building_brute_password(sevenzip_path, chars, password + c, len, file, one_file, final_password)) {
 			return true;
 		}
 	}
 	return false;
 }
+static bool decompress_zip(const std::string& sevenzip_path, const std::string& password, const std::string& file) {
+	std::string cmd = "\"" + sevenzip_path + " x -o\"" + file + "~\" -p\"" + password + "\" -y \"" + file + "\" > NUL 2>&1\"";
+	print(cmd);
+	int exit_code = system(cmd.c_str());
+	if (!exit_code) {
+		return true;
+	}
+	return false;
+}
 int main(int argc, char* argv[]) {
-	const std::string version = "2024.9.17";
+	const std::string version = "2024.9.22";
 	const std::string program_path = std::filesystem::absolute(argv[0]).parent_path().string();
 	const std::string sevenzip_path = "\"" + program_path + "/7z\"";
 	const std::string key_json_path = program_path + "/key.json";
@@ -67,12 +99,25 @@ int main(int argc, char* argv[]) {
 		std::cerr << file + " not found!" << std::endl;
 		return -114514;
 	}
+	std::string one_file = get_one_file_from_zip(sevenzip_path, file);
+	if (one_file == "") {
+		std::cerr << file + " is not a zip file!" << std::endl;
+		return -114514;
+	}
 	std::ifstream key_json_file(key_json_path);
 	nlohmann::json key_json_data = nlohmann::json::parse(key_json_file);
 	if (flag_exact) {
 		const std::vector<std::string>& exact_passwords = key_json_data["exact"];
 		for (const auto& password : exact_passwords) {
-			if (try_password(sevenzip_path, file, password)) {
+			if (try_password(sevenzip_path, file, password, one_file)) {
+				if (!decompress_zip(sevenzip_path, password, file)) {
+					if (std::filesystem::exists(file + "~")) {
+						std::filesystem::remove_all(file + "~");
+					}
+					std::cerr << "decompress failed!" << std::endl;
+					return -114514;
+				}
+				print("decompress success!");
 				return 0;
 			}
 		}
@@ -81,9 +126,18 @@ int main(int argc, char* argv[]) {
 		const size_t& min_len = key_json_data["brute"]["min_len"];
 		const size_t& max_len = key_json_data["brute"]["max_len"];
 		const std::string& chars = key_json_data["brute"]["chars"];
+		std::string password = "";
 		if (min_len <= max_len && chars != "") {
 			for (size_t len = min_len; len <= max_len; ++len) {
-				if (building_brute_password(sevenzip_path, chars, "", len, file)) {
+				if (building_brute_password(sevenzip_path, chars, "", len, file, one_file, password)) {
+					if (!decompress_zip(sevenzip_path, password, file)) {
+						if (std::filesystem::exists(file + "~")) {
+							std::filesystem::remove_all(file + "~");
+						}
+						std::cerr << "decompress failed!" << std::endl;
+						return -114514;
+					}
+					print("decompress success!");
 					return 0;
 				}
 			}
